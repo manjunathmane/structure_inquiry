898417c65ebe8a963c3cddb0a9687fd419b0c1a8
manjunathmane <manjunathmane38@gmail.com>
2026-01-05 16:38:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,19 @@
-from app import *
+from daily_calorie import calculate_daily_calories, calorie_status
 
-def test_discount():
-    price = 100
-    discount = 10
-    quantity = 2
-    assert (price * quantity) - ((price * quantity) * discount / 100) == 180
+def test_calorie_calculation():
+    age = 20
+    weight = 60
+    assert calculate_daily_calories(age, weight) == (60 * 24 - 20 * 3)
+
+def test_invalid_values():
+    assert calculate_daily_calories(0, 50) == 0
+    assert calculate_daily_calories(20, 0) == 0
+
+def test_high_calorie_status():
+    assert calorie_status(2600) == "High Calorie Requirement"
+
+def test_moderate_calorie_status():
+    assert calorie_status(2000) == "Moderate Calorie Requirement"
+
+def test_low_calorie_status():
+    assert calorie_status(1500) == "Low Calorie Requirement"

21b85aef09c30497f0f7ebf38e522b329c822b4b
manjunathmane <manjunathmane38@gmail.com>
2026-01-05 15:58:09 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
 from app import register_courses
 
 def test_course_registration():
-    student_name = "Alice"
+    student_name ="Alice"
     courses = [
-        {"course_name": "Python", "credits": 4},
-        {"course_name": "DevOps", "credits": 3}
+        {"course_name":"Python","credits":4},
+        {"course_name":"DevOps","credits":3}
     ]
 
     result = register_courses(student_name, courses)
 
-    assert result["student_name"] == "Alice"
-    assert len(result["courses"]) == 2
+    assert result["student_name"] =="Alice"
+    assert len(result["courses"]) ==2
